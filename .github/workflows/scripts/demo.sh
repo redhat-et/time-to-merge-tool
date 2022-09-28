10177609b447c4c10e27519b0789371fd2ca90d7
aakankshaduggal <aduggal@redhat.com>
2022-09-28 16:39:38 -0400

    Add bash script for setting up gh and repo to work with

---
+++ .github/workflows/scripts/demo.sh
@@ -0,0 +1,10 @@
+read -p 'Github username:' varname
+read -p 'Github password:' varpass
+
+read -p 'Repository name for TTM prediction:' varrepo
+
+echo
+echo "Thanks for setting up your github and sharing the repository you want to perform time-to-merge prediction for."
+echo "Now, let's fork the AI4CI repository"
+
+curl -u $varname https://api.github.com/repos/aicoe-aiops/ocp-ci-analysis/forks -d ''
