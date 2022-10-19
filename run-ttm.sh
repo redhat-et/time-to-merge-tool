a33e82799c100f49aa22c59ba234f18ab7d36b92
Oindrilla Chatterjee <oc@bu.edu>
2022-10-19 10:53:30 -0400

    added prefix to inputs

--- run-ttm.sh
+++ run-ttm.sh
@@ -1,12 +1,13 @@
-
 #!/bin/bash
 
-echo What is your github username?
+echo Enter your github username
 read UNAME
 echo Which repository do you want to train the time to merge model this on?
 read REPO
 echo Which Github organization or user is this repository a part of?
 read ORG
+echo What key/folder/prefix name do you want to store your trained model under?
+read PREFIX
 echo You will need to generate a Personal Access Token to run this tool. Enter your personal access token
 read PAT
 echo You will need S3 bucket credentials to store the model and datasets. You can configure your bucket credentials as Action secrets in your forked repository or provide them here. Do you want to share your bucket credentials here? y/n
@@ -25,7 +26,7 @@ then
   curl -X POST \
   -H 'authorization: Bearer '$PAT'' \
   https://api.github.com/repos/$UNAME/ttmtool/dispatches \
-  -d '{"event_type": "workflow-run", "client_payload":{"REPO":"'$REPO'", "ORG":"'$ORG'", "S3_BUCKET":"'$BUCKET'", "S3_ENDPOINT_URL":"'$S3ENDPOINT'", "AWS_ACCESS_KEY_ID":"'$ACCESSKEY'", "AWS_SECRET_ACCESS_KEY":"'$SECRETKEY'"}}'
+  -d '{"event_type": "workflow-run", "client_payload":{"REPO":"'$REPO'", "ORG":"'$ORG'", "PREFIX":"'$PREFIX'","S3_BUCKET":"'$BUCKET'", "S3_ENDPOINT_URL":"'$S3ENDPOINT'", "AWS_ACCESS_KEY_ID":"'$ACCESSKEY'", "AWS_SECRET_ACCESS_KEY":"'$SECRETKEY'"}}'
 
 else
   echo Please add your S3 bucket credentials as repository action secrets
@@ -33,7 +34,7 @@ else
   curl -X POST \
   -H 'authorization: Bearer '$PAT'' \
   https://api.github.com/repos/$UNAME/ttmtool/dispatches \
-  -d '{"event_type": "workflow-run", "client_payload":{"REPO":"'$REPO'", "ORG":"'$ORG'"}}'
+  -d '{"event_type": "workflow-run", "client_payload":{"REPO":"'$REPO'", "ORG":"'$ORG'", "PREFIX":"'$PREFIX'"}}'
 fi
 
 echo Request sent. Check github action workflow for running workflow
