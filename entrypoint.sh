e9397372042391f7568417ca647756d42b5d88b7
Aakanksha Duggal <aduggal@redhat.com>
2022-11-11 15:44:37 -0500

    cleanup action test (#16)
    * Update action.yml
    
    * Update action.yml
    
    * Update entrypoint.sh
    
    * Update action.yml
    
    * Update entrypoint.sh
    
    Co-authored-by: Michael Clifford <mcliffor@redhat.com>
    
    * Update inference.yaml
    
    * Update action.yml
    
    * Rename Containerfile to Dockerfile
    
    Co-authored-by: Michael Clifford <mcliffor@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+# Test run for model inference notebook
 
-
-jupyter nbconvert --to notebook --execute test.ipynb --TemplateExporter.exclude_input=True \
- --ExecutePreprocessor.kernel_name='python3' --output test_notebook_executed
+jupyter nbconvert --to notebook --execute /04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
+ --ExecutePreprocessor.kernel_name='python3' --output 04_model_inference_executed  
