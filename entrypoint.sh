81caa11b6d584767331b162066c6c0168c497bc1
Aakanksha Duggal <aduggal@redhat.com>
2022-11-11 15:27:09 -0500

    Update entrypoint.sh
    Co-authored-by: Michael Clifford <mcliffor@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Test run for model inference notebook
 
-jupyter nbconvert --to notebook --execute 04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
+jupyter nbconvert --to notebook --execute /04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
  --ExecutePreprocessor.kernel_name='python3' --output 04_model_inference_executed  
