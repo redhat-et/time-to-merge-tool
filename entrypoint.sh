d0327bc46eed4c778459068599bd491ac9b7e941
Aakanksha Duggal <aduggal@redhat.com>
2022-11-22 14:22:18 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
-# Test run for model inference notebook
+# If mode is 1, run training + inference mode, otherwise just run the inference
 
-jupyter nbconvert --to notebook --execute /04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
+if [ $MODE==1]
+then 
+ # Data collection
+ python 01_data_collection.py
+ 
+ # Feature Engineering
+ jupyter nbconvert --to notebook --execute 02_feature_engineering.ipynb --TemplateExporter.exclude_input=True \
+ --ExecutePreprocessor.kernel_name='python3' --output 02_notebook_executed
+ 
+ # Model Training
+ jupyter nbconvert --to notebook --execute 03_model_training.ipynb --TemplateExporter.exclude_input=True \
+ --ExecutePreprocessor.kernel_name='python3' --output 03_notebook_executed
+ 
+ # Model Inference
+  jupyter nbconvert --to notebook --execute /04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
  --ExecutePreprocessor.kernel_name='python3' --output 04_model_inference_executed  
+
+else
+  # Since the mode wasn't specified just run the model inference on new pull request
+  jupyter nbconvert --to notebook --execute /04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
+ --ExecutePreprocessor.kernel_name='python3' --output 04_model_inference_executed
+fi
