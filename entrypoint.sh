9ad4d0aa6cc92de8e00c218d23961b8587b460c5
Aakanksha Duggal <aduggal@redhat.com>
2022-11-30 11:21:34 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 # If mode is 1, run training + inference mode, otherwise just run the inference
 
-if (( $MODE==1 ))
-then 
- echo "Training Mode"
- # Data collection
- python 01_data_collection.py
- 
- # Feature Engineering
- jupyter nbconvert --to notebook --execute 02_feature_engineering.ipynb --TemplateExporter.exclude_input=True \
+if [ $MODE==1 ]
+then
+   # Data collection
+   python 01_data_collection.py
+   
+   # Feature Engineering
+   jupyter nbconvert --to notebook --execute 02_feature_engineering.ipynb --TemplateExporter.exclude_input=True \
  --ExecutePreprocessor.kernel_name='python3' --output 02_notebook_executed
- 
- # Model Training
- jupyter nbconvert --to notebook --execute 03_model_training.ipynb --TemplateExporter.exclude_input=True \
+   
+   # Model Training
+   jupyter nbconvert --to notebook --execute 03_model_training.ipynb --TemplateExporter.exclude_input=True \
  --ExecutePreprocessor.kernel_name='python3' --output 03_notebook_executed
- 
- # Model Inference
-  jupyter nbconvert --to notebook --execute /04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
- --ExecutePreprocessor.kernel_name='python3' --output 04_model_inference_executed  
+   
+   # Model Inference
+   jupyter nbconvert --to notebook --execute 03_model_training.ipynb --TemplateExporter.exclude_input=True \
+ --ExecutePreprocessor.kernel_name='python3' --output 03_notebook_executed
 
 else
-  echo "Inference Mode"
-  # Since the mode wasn't specified just run the model inference on new pull request
-  jupyter nbconvert --to notebook --execute /04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
+   if [ $MODE==0 ]
+   then
+      jupyter nbconvert --to notebook --execute /04_model_inference.ipynb  --TemplateExporter.exclude_input=True \
  --ExecutePreprocessor.kernel_name='python3' --output 04_model_inference_executed
+   fi
 fi
